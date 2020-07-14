88c1664e1551a4b84ac01465f154aa8a0f6762fd
oğulcan <karayel.ogulcan1903@gmail.com>
2020-07-14 18:23:39 +0300

    gamescreen added

--- gameData.js
+++ gameData.js
@@ -7,13 +7,21 @@ export default games = [
         category:["Action","Adventure"],
         age:"13+",
         cover:require("./assets/valhalla.jpg"),
+        screenshots:[
+            require("./assets/v1.jpg"),
+            require("./assets/v2.jpg")
+        ],
         backgroundColor:"#824571"
     },
     {
         id:2,
         title:"Assasins Creed Valhalla",
         description:"The last game of assasins creed",
-        rating:9.2,
+        rating:3.2,
+        screenshots:[
+            require("./assets/v1.jpg"),
+            require("./assets/v2.jpg")
+        ],
         category:["Action","Adventure"],
         age:"13+",
         cover:require("./assets/valhalla.jpg"),
